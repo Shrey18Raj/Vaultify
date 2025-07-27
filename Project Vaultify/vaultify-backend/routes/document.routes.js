a2200ba161009c067a91f2894f3da76083725296
Shrey18Raj <shreyanshraj.18@gmail.com>
2025-07-27 20:22:59 +0530

    Restore document.routes.js changes for 2025-07-27

---
+++ Project Vaultify/vaultify-backend/routes/document.routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const DocumentController = require('../controllers/document.controller');
+const authenticateToken = require('../middlewares/auth.middlewares');
+
+router.post('/upload', authenticateToken, DocumentController.uploadFile);
+
+router.get('/my-documents', authenticateToken, DocumentController.getMyDocuments);
+
+router.get('/download/:filename', authenticateToken, DocumentController.downloadFile);
+
+router.get('/shared-access/:token', DocumentController.sharedAccess);
+
+router.delete('/delete/:filename', authenticateToken, DocumentController.deleteFile);
+
+router.get('/stats', authenticateToken, DocumentController.getDashboardStats);
+
+router.get('/by-folder/:folderId', authenticateToken, DocumentController.getDocumentsByFolder)
+
+router.post('/secure-share/:id', authenticateToken, DocumentController.generateAdvancedShareToken);
+
+router.post('/secure-access/:token', DocumentController.sharedAccess)
+
+module.exports = router;
