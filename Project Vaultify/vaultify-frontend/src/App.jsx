7be82f9cf9bb3a216a0d4d929c53ba1ff267b39e
Shrey18Raj <shreyanshraj.18@gmail.com>
2025-07-30 17:56:13 +0530

    Restore App.jsx changes for 2025-07-30

--- Project Vaultify/vaultify-frontend/src/App.jsx
+++ Project Vaultify/vaultify-frontend/src/App.jsx
@@ -5,22 +5,37 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProtectedRoute from './routes/ProtectedRoute';
 import Dashboard from './pages/Dashboard';
-import Sidebar from './components/Sidebar';
-import DashboardHome from './pages/DashboardHome';
+import DashboardLayout from './layout/DashboardLayout';
+import MyDocuments from './pages/MyDocuments';
+import UploadDocuments from './pages/UploadDocument';
+import { AuthProvider } from './context/AuthProvider';
+import DocumentDetail from './pages/DocumentDetail';
+import FolderView from './pages/FolderView';
 
 export default function App() {
     return (
       <Router>
-          <Routes>
+        <AuthProvider>
+            <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
-                <Route element={<ProtectedRoute />}>
-                    <Route path="/dashboard" element={<Sidebar />}>
-                        <Route index element={<DashboardHome />} />
-                    </Route>               
+
+                <Route element={<ProtectedRoute />}>   
+                    <Route element={<DashboardLayout />}>
+                        <Route path='/dashboard' element={<Dashboard />} />
+                            <Route path="/dashboard" element={<div>Welcome to Vaultify Dashboard</div>} />
+                            <Route path="/documents" element={<MyDocuments />} />
+                            <Route path="/documents/upload" element={<UploadDocuments />} />
+                            <Route path='/documents/:id' element={<DocumentDetail />} />
+                            <Route path="/folders" element={<FolderView />} />
+                            <Route path="/sharing" element={<div>Sharing Interface</div>} />
+                            <Route path="/analytics" element={<div>Analytics Overview</div>} />
+                            <Route path="/settings" element={<div>Account & Settings</div>} />
+                    </Route>        
                 </Route>
-          </Routes>
+            </Routes>
+        </AuthProvider>
       </Router>
     );
 }
