d5a26e83e1cc9890097daaff2d5d045d01262c97
Shrey18Raj <shreyanshraj.18@gmail.com>
2025-08-02 22:08:28 +0530

    Restore App.jsx changes for 2025-08-02

--- Project Vaultify/vaultify-frontend/src/App.jsx
+++ Project Vaultify/vaultify-frontend/src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
