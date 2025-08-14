a7786c5c13fbd025a90e9a8e687aef5288cebebf
Shrey18Raj <165501450+Shrey18Raj@users.noreply.github.com>
2025-08-14 06:00:47 +0530

    Delete duplicate directory

--- Project Vaultify/vaultify-backend/routes/dashboard.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const DashboardController = require('../controllers/dashboard.controller');
-const authenticateToken = require('../middlewares/auth.middlewares');
-
-router.get('/overview', authenticateToken, DashboardController.getOverview);
-
-module.exports = router;
